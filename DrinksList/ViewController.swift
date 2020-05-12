05d87c41803b6afc2067244171d9d22c08d4cea6
ericksilva-dev <erick.kaique.silva272@gmail.com>
2020-05-12 00:19:20 -0300

    Started Project

---
+++ DrinksList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrinksList
+//
+//  Created by Erick Kaique Da Silva on 12/05/20.
+//  Copyright © 2020 Erick Kaique Da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
